da84018fa35d1d505ca8ac920cd85429806f84a6
Irvin Lim <irvinlimweiquan@gmail.com>
2017-11-21 06:07:22 +0800

    Initial commit from electron-vue boilerplate generator

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2017,
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+        },
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    globals: {
+        __static: true,
+    },
+    plugins: ['html', 'prettier'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-console': 'off',
+
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        // allow async-await
+        'generator-star-spacing': 0,
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    },
+};
---
+++ src/renderer/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'electron-vue',
+};
+</script>
+
+<style>
+@import '~bulma';
+html,
+body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    display: flex;
+    height: 100%;
+    flex-direction: row;
+    align-items: stretch;
+}
+</style>
