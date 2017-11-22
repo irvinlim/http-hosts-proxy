d1eb8d50299ecbd90b9daa5b86c50b1724e82c20
Irvin Lim <irvinlimweiquan@gmail.com>
2017-11-23 04:16:41 +0800

    Add comments to eslintrc.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,16 @@ module.exports = {
   },
   plugins: ['html', 'prettier', 'mocha'],
   rules: {
+    // always use LF for line breaks
     'linebreak-style': ['error', 'unix'],
+
+    // always use single quotes
     quotes: ['error', 'single'],
+
+    // always use semicolons
     semi: ['error', 'always'],
+
+    // allow console methods
     'no-console': 'off',
 
     // don't allow redeclaration of vars in outer scope
@@ -30,12 +37,15 @@ module.exports = {
       { allow: ['resolve', 'reject', 'done', 'cb', 'event', 'err'] },
     ],
 
+    // don't allow unused vars
     'no-unused-vars': ['error', { varsIgnorePattern: '^_.*' }],
 
     // allow paren-less arrow functions
     'arrow-parens': 0,
+
     // allow async-await
     'generator-star-spacing': 0,
+
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
   },
